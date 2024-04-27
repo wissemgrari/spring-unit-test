84345b29c531f3ea73a7cc1db994a5c1680a540f
wissemgrari <wissemgrari@outlook.com>
2024-04-27 18:25:13 +0100

    initial commit

---
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -0,0 +1,57 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Pokemon;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class PokemonRepositoryTests {
+  
+  @Autowired private PokemonRepository pokemonRepository;
+  
+  @Test
+  public void PokemonRepository_SaveAll_ReturnSavedPokemon() {
+    
+    //Arrange
+    Pokemon pokemon = Pokemon.builder().name("pikachu").type("electric").build();
+    
+    //Act
+    Pokemon savedPokemon = pokemonRepository.save(pokemon);
+    
+    //Assert
+    Assertions.assertThat(savedPokemon).isNotNull();
+    Assertions.assertThat(savedPokemon.getId()).isGreaterThan(0);
+  }
+  
+  @Test
+  public void PokemonRepository_GetAll_ReturnMoreThenOnePokemon() {
+    Pokemon pokemon1 = Pokemon.builder().name("pikachu").type("electric").build();
+    Pokemon pokemon2 = Pokemon.builder().name("pikachu").type("electric").build();
+    
+    pokemonRepository.save(pokemon1);
+    pokemonRepository.save(pokemon2);
+    
+    List<Pokemon> pokemonList = pokemonRepository.findAll();
+    Assertions.assertThat(pokemonList).isNotNull();
+    Assertions.assertThat(pokemonList.size()).isEqualTo(2);
+    
+  }
+  
+  @Test
+  public void PokemonRepository_FindById_ReturnPokemon() {
+    Pokemon pokemon = Pokemon.builder().name("pikachu").type("electric").build();
+    
+    pokemonRepository.save(pokemon);
+    
+    Pokemon returnedPokemon = pokemonRepository.findById(pokemon.getId()).get();
+    
+    Assertions.assertThat(returnedPokemon.getId()).isNotNull();
+  }
+}
